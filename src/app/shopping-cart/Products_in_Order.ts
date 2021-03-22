48dd0948aed078011681e05b4e2980e3e0791506
einavKatav <katave@post.bgu.ac.il>
2021-03-22 13:07:44 +0200

    cant see the products if not signed in

--- src/app/shopping-cart/Products_in_Order.ts
+++ src/app/shopping-cart/Products_in_Order.ts
@@ -1,5 +1,7 @@
-import { IdEntity, StringColumn, EntityClass, NumberColumn, DateColumn, IdColumn, BoolColumn } from '@remult/core';
-
+// import { IdEntity, StringColumn, EntityClass, NumberColumn, DateColumn, IdColumn, BoolColumn } from '@remult/core';
+import { IdEntity, StringColumn, EntityClass, NumberColumn, DateColumn, IdColumn, BoolColumn, Context } from '@remult/core';
+import { Roles } from '../users/roles';
+import { sneUserInfo } from '../users/users';
 @EntityClass
 export class Products_in_Order extends IdEntity {
     Product_ID = new IdColumn();
